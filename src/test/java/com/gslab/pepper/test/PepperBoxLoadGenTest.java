18ba905e2f797deb5e7792b43d5bb46435cec392
marcocata-qy <marco.catalano@quantyca.it>
2019-06-06 14:16:42 +0200

    Removed unused statement

--- src/test/java/com/gslab/pepper/test/PepperBoxLoadGenTest.java
+++ src/test/java/com/gslab/pepper/test/PepperBoxLoadGenTest.java
@@ -55,7 +55,6 @@ public class PepperBoxLoadGenTest {
         KafkaConfig config = new KafkaConfig(brokerProps);
         Time mock = new MockTime();
         kafkaServer = TestUtils.createServer(config, mock);
-        //AdminUtils.createTopic(zkUtils, TOPIC, 1, 1, new Properties(), RackAwareMode.Disabled$.MODULE$);
 
         JMeterContext jmcx = JMeterContextService.getContext();
         jmcx.setVariables(new JMeterVariables());
